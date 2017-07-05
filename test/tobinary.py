2aba7cceb909e0d889b789c7749c9675d4bae3b1
xing yan <zgyan012@gmail.com>
2017-07-05 12:31:06 +0800

    add file: tobinary

--- test/tobinary.py
+++ test/tobinary.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 
 
-def tobinary(num):
-    pass
+def tobinary(num, binary_list=None):
+    if binary_list is None:
+        binary_list = []
 
+    if num == 0:
+        return 0
+
+    binary_list.insert(0, num % 2)
+
+    if num // 2 == 0:
+        return binary_list
+
+    return tobinary(num // 2, binary_list)
+
+
+print(tobinary(7))
